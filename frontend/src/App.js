68ae6e5d2aaeae77634af941ce03a25230028b07
valerieli <valerieli@berkeley.edu>
2023-10-29 13:16:25 -0700

    added recipe page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import ImportForm from './ImportForm';
 import DishSelect from './DishSelect';
+import RecipeSelect from './RecipeSelect';
 
 function App() {
   const [response, setResponse] = useState("");
   const [dishes, setDishes] = useState({});
   const [loaded, setLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [recipe, setRecipe] = useState([]);
 
   return (
     <div className="App">
@@ -17,6 +19,7 @@ function App() {
         ? <DishSelect dishes={dishes} setLoaded={setLoaded}/> 
         : <ImportForm setDishes={setDishes} setLoaded={setLoaded} setLoading={setLoading}/>}
     </div>
+
   );
 }
 
--- frontend/src/DishSelect.js
+++ frontend/src/DishSelect.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export default function DishSelect(props) {
     const {dishes, setLoaded} = props;
-
+    //const {dishes, setSelectedDish} = props;
     return (
         <div>
             <h1>
